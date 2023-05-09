c545d692e1d7bc4fe5860feaba077a9018d2b967
ysgorin <ysgorin@gmail.com>
2023-05-08 21:50:28 -0400

    added double quotes to film ID

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 -- Create the Superhero Movie Table Schema
 CREATE TABLE Movies (
-    Film ID INT,
+    "Film ID" INT,
     Title VARCHAR NOT NULL,
     Year INT NOT NULL,
     "Runtime (min)" INT NOT NULL,
     "Rotten Tomatoes Rating (%)" INT NOT NULL,
     "Box Office ($)" INT NOT NULL,
     Franchise VARCHAR(4),
-    PRIMARY KEY (Film ID)
+    PRIMARY KEY ("Film ID")
 );
