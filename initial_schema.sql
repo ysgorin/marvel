6db73138a88bd3606ad84d84fdf979daa2c940a0
ysgorin <ysgorin@gmail.com>
2023-05-09 22:27:55 -0400

    added sql schema for uncleaned data

---
+++ initial_schema.sql
@@ -0,0 +1,17 @@
+-- MCU Table
+CREATE TABLE mcu(
+    Title VARCHAR NOT NULL,
+    Year VARCHAR NOT NULL,
+    Runtime VARCHAR NOT NULL,
+    "Rotten Tomatoes Rating" VARCHAR NOT NULL,
+    "Box Office" VARCHAR NOT NULL
+);
+
+-- DCEU Table
+CREATE TABLE dceu(
+    Title VARCHAR NOT NULL,
+    Year VARCHAR NOT NULL,
+    Runtime VARCHAR NOT NULL,
+    "Rotten Tomatoes Rating" VARCHAR NOT NULL,
+    "Box Office" VARCHAR NOT NULL
+);
