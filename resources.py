9eff4753d91a94025e29e55959f5dd45fc4effb3
ysgorin <ysgorin@gmail.com>
2023-07-13 09:43:06 -0400

    added two movies to resource.py

--- resources.py
+++ resources.py
@@ -8,9 +8,9 @@ mcu_movie_list = ['Iron Man', 'The Incredible Hulk', 'Iron Man 2', 'Thor',
                   'Spider-Man: Far From Home', 'Black Widow', 'Shang-Chi and the Legend of the Ten Rings',
                   'Eternals', 'Spider-Man: No Way Home', 'Doctor Strange in the Multiverse of Madness',
                   'Thor: Love and Thunder', 'Black Panther: Wakanda Forever',
-                  'Ant-Man and the Wasp: Quantumania']
+                  'Ant-Man and the Wasp: Quantumania', 'Guardians of the Galaxy Vol. 3']
 
 dceu_movie_list = ['Man of Steel', 'Batman v Superman: Dawn of Justice', 'Suicide Squad', 'Wonder Woman',
                    'Justice League', 'Aquaman', 'Shazam!', 'Birds of Prey', 'Wonder Woman 1984',
                    "Zack Snyder's Justice League", 'The Suicide Squad', 'Black Adam',
-                   'Shazam! Fury of the Gods']
+                   'Shazam! Fury of the Gods', 'The Flash']
